789b6864411401f870c663c6b71849ad8c069bd2
tylersqors <tyler.otto@sqor.com>
2014-02-17 13:15:08 -0800

    updated

---
+++ Lesson5_5.py
@@ -0,0 +1,72 @@
+# -----------
+# User Instructions
+#
+# Define a function smooth that takes a path as its input
+# (with optional parameters for weight_data, weight_smooth)
+# and returns a smooth path.
+#
+# Smoothing should be implemented by iteratively updating
+# each entry in newpath until some desired level of accuracy
+# is reached. The update should be done according to the
+# gradient descent equations given in the previous video:
+#
+# If your function isn't submitting it is possible that the
+# runtime is too long. Try sacrificing accuracy for speed.
+# -----------
+
+
+from math import *
+
+# Don't modify path inside your function.
+path = [[0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+        [4, 3],
+        [4, 4]]
+
+# ------------------------------------------------
+# smooth coordinates
+#
+
+def smooth(path, weight_data = 0.5, weight_smooth = 0.1):
+
+    # Make a deep copy of path into newpath
+    newpath = [[0 for col in range(len(path[0]))] for row in range(len(path))]
+    tolerance = 0.00001
+    for i in range(len(path)):
+        for j in range(len(path[0])):
+            newpath[i][j] = path[i][j]
+
+
+    #### ENTER CODE BELOW THIS LINE ###
+    Alpha = weight_data
+    Beta = weight_smooth
+    tol=1000
+    while tol >= tolerance:
+        for i in range(len(path)):
+            if i == 0 or i == (len(path)-1):
+                pass
+            else:
+                for j in range(len(path[0])):
+                    compare = newpath[i][j]
+                    newpath[i][j] += Alpha*(path[i][j]-newpath[i][j])
+                    newpath[i][j] += Beta*(newpath[i+1][j]+newpath[i-1][j]-2.0*newpath[i][j])
+                    tol = abs(compare-newpath[i][j])
+    
+    return newpath # Leave this line for the grader!
+
+# feel free to leave this and the following lines if you want to print.
+newpath = smooth(path)
+
+# thank you - EnTerr - for posting this on our discussion forum
+for i in range(len(path)):
+    print '['+ ', '.join('%.3f'%x for x in path[i]) +'] -> ['+ ', '.join('%.3f'%x for x in newpath[i]) +']'
+
+
+
+
+
--- motion_planning.py
+++ motion_planning.py
@@ -1,89 +1,99 @@
-# ----------
+# -----------
 # User Instructions:
 # 
-# Define a function, search() that takes no input
-# and returns a list
-# in the form of [optimal path length, x, y]. For
-# the grid shown below, your function should output
-# [11, 4, 5].
+# Modify the function search() so that it returns
+# a table of values called expand. This table
+# will keep track of which step each node was
+# expanded.
 #
-# If there is no valid path from the start point
-# to the goal, your function should return the string
-# 'fail'
+# For grading purposes, please leave the return
+# statement at the bottom.
 # ----------
 
-# Grid format:
-#   0 = Navigable space
-#   1 = Occupied space
 
-grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 0]]
 
-init = [0, 0]
-goal = [len(grid)-1, len(grid[0])-1] # Make sure that the goal definition stays in the function.
+init = [0, 2]
+goal = [len(grid)-1, len(grid[0])-1]
 
 delta = [[-1, 0 ], # go up
-        [ 0, -1], # go left
-        [ 1, 0 ], # go down
-        [ 0, 1 ]] # go right
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
 cost = 1
 
 
+# ----------------------------------------
+# modify code below
+# ----------------------------------------
+
 def search():
-    grid1 = grid
-    location_set = [[0,init[0],init[1]]] 
-    grid1[init[0]][init[1]] = 1
-    test = False
-    i=0
-    while test == False:
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
 
-        for location in location_set:
-            location_set.remove(location)
-            possible_motion = motion(location, grid1)
-            # print "&&&"
+    x = init[0]
+    y = init[1]
+    g = 0
 
-            for test_location in possible_motion:
-                if test_location[1:3] == goal:
-                    test = True
-                    return test_location
-                else:
-                    test = False
-                    location_set.append(test_location)
-        # print location_set
+    open = [[g, x, y]]
 
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+    
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    expand[init[0]][init[1]] = 0
+    j = 0
+    
+    motion = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    # motion[init[0]][init[1]] = 0
 
+    while not found and open!=[]:
+        open.sort()
+        open.reverse()
+        next = open.pop()
+        x = next[1]
+        y = next[2]
+        g = next[0]
 
-def motion(location, grid):
-    # print "***"
-    # print location
-    possible_motion = []
-
-    for motion in delta:
+        if next == []:
+            found = True
+       
+        # expand[next[1]][next[2]] = g
+        # j+=1
         
-        new_location = ["",""]
-        new_location[0]=location[1]+motion[0]
-        new_location[1]=location[2]+motion[1]
-        # print new_location
+        # if x == goal[0] and y == goal[1]:
+        #     found = True
+            # print next
+        else:
+            for i in range(len(delta)):
+                expand[next[1]][next[2]] = g
+                j+=1
+                x2 = x + delta[i][0]
+                y2 = y + delta[i][1]
+                if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                    if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                        g2 = g + cost
+                        open.append([g2, x2, y2])
+                        # motion[x][y] = 
+                        closed[x2][y2] = delta_name[i]
 
-        if (len(grid)-1) >= new_location[0]>=0 and (len(grid[0])-1)>=new_location[1]>=0:
-            if grid[new_location[0]][new_location[1]] == 0:
-                possible_motion.append([location[0]+1, new_location[0], new_location[1]])
-                grid[new_location[0]][new_location[1]] = 1
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if expand[i][j]==-1:
+                expand[i][j]=99
+                            
+    print expand
+    return expand
+    # return expand #Leave this line for grading purposes!
 
-    # print possible_motion
-    # print "!!!"
-    # print possible_motion
-    return possible_motion
+search()
 
-def test(location):
-    if location == goal:
-        return 'goal'
-    else:
-        return 'keep going'
+
 
